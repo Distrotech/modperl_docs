d696cb3b7e4e757e6bf70d36f15788aba6ee2da1
stas <stas@13f79535-47bb-0310-9956-ffa450edef68>
2002-03-22 10:54:22 +0000

    last env tweaks
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/docs/trunk@65997 13f79535-47bb-0310-9956-ffa450edef68

--- src/search/apache.org-setup.pl
+++ src/search/apache.org-setup.pl
@@ -1,14 +1,13 @@
-require Sys::Hostname;
-my $hostname = Sys::Hostname::hostname();
-
 # to use the setup at home, simply add to httpd.conf:
 # SetEnv SWISH_BINARY_PATH /usr/local/bin/swish-e
 # adjust to the real path
 
 # on daedalus (the production server) we cannot modify the config file
 # so we do it here
-
 BEGIN {
+    require Sys::Hostname;
+    my $hostname = Sys::Hostname::hostname();
+
     if ($hostname && $hostname eq 'daedalus.apache.org') {
         $ENV{SWISH_BINARY_PATH} = "/home/stas/bin/swish-e";
 
