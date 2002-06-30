7b0f0361d3fb78625c6535eddd197217ed479242
stas <stas@13f79535-47bb-0310-9956-ffa450edef68>
2002-06-30 16:45:34 +0000

    add the missing categories add the docs subsection's versioning more explicit
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/docs/trunk@66576 13f79535-47bb-0310-9956-ffa450edef68

--- src/search/make.pl
+++ src/search/make.pl
@@ -8,12 +8,12 @@ make.pl -- program to generate data needed for searching
 
 =head1 Description
 
-make.pl uses input contained within that defines "sections" of the site based
-on path names.  These name can then be used when searching with swish to limit
-searches to just these areas of the site.
+make.pl uses input contained within that defines "sections" of the
+site based on path names.  These name can then be used when searching
+with swish to limit searches to just these areas of the site.
 
-When indexing the site with swish-e each file is taged with meta data that indicates
-which section or sections it belongs to.
+When indexing the site with swish-e each file is taged with meta data
+that indicates which section or sections it belongs to.
 
 The input format is described in the source of this file.
 
@@ -23,25 +23,28 @@ make.pl creates two ouptut files:
 
 =item search_options
 
-A template toolkit include file for defining an array of section names and a hash that
-maps the section names to nice descriptions.  This data is used to
-create the select box on the side bar during site generation (by running bin/build).
+A template toolkit include file for defining an array of section names
+and a hash that maps the section names to nice descriptions.  This
+data is used to create the select box on the side bar during site
+generation (by running bin/build).
 
-It also creates a hash to use in a TT plugin to map the file's path while running bin/build into
-section IDs.
+It also creates a hash to use in a TT plugin to map the file's path
+while running bin/build into section IDs.
 
 =item checkboxes.storable
 
-A perl data structure used for use in the F<search.cgi> script to generate the nested
-checkboxes for the advanced search feature.  This allows selecting more than one
-area of the site at a time.
+A perl data structure used for use in the F<search.cgi> script to
+generate the nested checkboxes for the advanced search feature.  This
+allows selecting more than one area of the site at a time.
 
-This file is saved using the Storable perl module, and is read in by the
-search script (F<swish.cgi>) configuration parameter file F<.swishcgi.conf> and
-made available to Template-Toolkit when F<swish.cgi> is running.
+This file is saved using the Storable perl module, and is read in by
+the search script (F<swish.cgi>) configuration parameter file
+F<.swishcgi.conf> and made available to Template-Toolkit when
+F<swish.cgi> is running.
 
-This file is also read when indexing with swish-e (see F<SwishSpiderConfig.pl>) and is used to
-map path names into section names.
+This file is also read when indexing with swish-e (see
+F<SwishSpiderConfig.pl>) and is used to map path names into section
+names.
 
 =back
 
@@ -52,7 +55,8 @@ the F<src/search> directory of the mod_perl site distribution.
 =cut
 
 
-# This must match up with .swishcgi.conf setting and SwishSpiderConfig.pl
+# This must match up with .swishcgi.conf setting and
+# SwishSpiderConfig.pl
 my $CHECKBOX_DATA = 'checkboxes.storable';
 
 # This is used for all pages -- it's the array and has for the sidebar search
@@ -61,24 +65,25 @@ my $SEARCH_OPTIONS = 'search_options';
 
 
 # Stas added tree display - Apr 15, 2002
-# Rewritten May 23, 2000 at Stas' request to centralize the input data in one place
+# Rewritten May 23, 2000 at Stas' request to centralize the input data
+# in one place
 # syntax (amount of spaces doesn't matter):
 # indent, path, title, optional short title (for drop down list)
 #
 
-
 my $items = <<ITEMS;
+    0, start,              What's mod_perl?
     0, outstanding,        Technologie Extraordinaire, Stories
     0, download,           Download,                   Download
     0, docs,               Documentation,              All Docs
     1,   docs/1.0,         mod_perl 1.0 Docs,          1.0 Docs
-    2,     docs/1.0/guide, Guide
-    2,     docs/1.0/win32, Win32
-    2,     docs/1.0/api,   API
+    2,     docs/1.0/guide, Guide (1.0)
+    2,     docs/1.0/win32, Win32 (1/.0)
+    2,     docs/1.0/api,   API (1.0)
     1,   docs/2.0,         mod_perl 2.0 Docs,          2.0 Docs
-    2,     docs/2.0/user,  User
-    2,     docs/2.0/devel, Developer
-    2,     docs/2.0/api,   API
+    2,     docs/2.0/user,  User (2.0)
+    2,     docs/2.0/devel, Developer (2.0)
+    2,     docs/2.0/api,   API (2.0)
     1,   docs/general,     General Docs
     1,   docs/tutorials,   Tutorials
     1,   docs/offsite,     OffSite Docs
@@ -86,6 +91,7 @@ my $items = <<ITEMS;
     0, maillist,           Mailing Lists
     0, products,           Products
     0, contribute,         Contribute
+    0, about,              About mod_perl
 ITEMS
 
 
