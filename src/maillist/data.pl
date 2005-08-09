a7eec851b66905fac9cd58b7523e42f91cd760e1
pgollucci <pgollucci@13f79535-47bb-0310-9956-ffa450edef68>
2005-08-09 06:40:01 +0000

    fix the url for Apache-Test
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/docs/trunk@230993 13f79535-47bb-0310-9956-ffa450edef68

--- src/maillist/data.pl
+++ src/maillist/data.pl
@@ -253,7 +253,7 @@ DESC
       title => 'Apache-Test Development',
       desc  => <<"DESC",
 The B<test-dev> mailing list is for discussing I<Apache-Test>
-L<http://perl.apache.org/test/> project.
+L<http://perl.apache.org/Apache-Test> project.
 DESC
       addr  => {
                 subscribe        => 'test-dev-subscribe@perl.apache.org',
