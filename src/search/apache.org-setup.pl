c5b9dd165df9e64d32f6811fea541d73e1dc7b53
pgollucci <pgollucci@13f79535-47bb-0310-9956-ffa450edef68>
2009-05-13 02:44:26 +0000

    another place to change and update for this decade
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/docs/trunk@774172 13f79535-47bb-0310-9956-ffa450edef68

--- src/search/apache.org-setup.pl
+++ src/search/apache.org-setup.pl
@@ -8,19 +8,8 @@ BEGIN {
     require Sys::Hostname;
     my $hostname = Sys::Hostname::hostname();
 
-    if ($hostname && ($hostname eq 'minotaur.apache.org' ||
-                      $hostname eq 'daedalus.apache.org')) {
-        $ENV{SWISH_BINARY_PATH} = "/home/perlwww/bin/swish-e";
-
-        unshift @INC,
-            qw(
-               /home/perlwww/lib/perl5/5.00503
-               /home/perlwww/lib/perl5/site_perl/5.005
-               /home/perlwww/lib/perl5/site_perl/
-               /home/perlwww/lib/perl5/site_perl/
-               /home/perlwww/lib/perl5/
-              );
-
+    if ($hostname && ($hostname eq 'minotaur.apache.org')) {
+        $ENV{SWISH_BINARY_PATH} = "/usr/local/bin/swish-e";
     }
 }
 
